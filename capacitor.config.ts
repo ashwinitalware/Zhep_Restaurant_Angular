4c13f17fe1cc7f932b6d7368eea9e27bb157761e
Ashwini Talware <81010617+ashwinitalware@users.noreply.github.com>
2023-12-14 14:52:13 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Zhep_Restaurant_Angular',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
