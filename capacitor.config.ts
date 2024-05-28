b54a4d1c03dac3a361e9a0ed1211888a6e1eb23f
Ashwini Talware <81010617+ashwinitalware@users.noreply.github.com>
2024-05-28 16:22:28 +0530

    working on splash screen and profiel page

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.zheprestaurant.com',
   appName: 'Zhep_Restaurant_Angular',
   webDir: 'www',
   server: {
